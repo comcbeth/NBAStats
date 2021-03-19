55e5c5cc49cbc551f97eb36594ea20231d5f9853
comc <63000898+comc@users.noreply.github.com>
2021-03-19 02:38:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,43 @@ import json
 
 #LINK TO API DOCUMENTATION: https://www.balldontlie.io/?shell#introduction
 
-search = input()
 
-url = "https://www.balldontlie.io/api/v1/players?search="+str(search)
-url2 = "https://www.balldontlie.io/api/v1/season_averages?"
+playerUrl = "https://www.balldontlie.io/api/v1/players?search="
+averagesUrl = "https://www.balldontlie.io/api/v1/season_averages?season=2020&player_ids[]="
 
-response = requests.get(url)
-sc = response.status_code
+
+candidates = ["Joel Embiid", "Nikola Jokic", "Lebron James", "Damian Lillard", "Luka Doncic", "Stephen Curry", "Giannis Antetokounmpo", "James Harden", "Kawhi Leonard", "Donovan Mitchell"]
+ids = []
+seasonScores= []
+mvpscore = 0
 
 def formatjson(obj):
     text = json.dumps(obj.json(), sort_keys=True, indent=4)
     return(text)
-    #create a formatted string of the Python JSON object
+    
 
-NBAData = formatjson(response)
-# player = NBAData['data'][0]
-player = NBAData
-print(player)
+for player in candidates:
+    response = requests.get(playerUrl+str(player))
+    sc = response.status_code
+    if sc == 200:
+        player = json.loads(formatjson(response))['data'][0]
+        ids.append(player['id'])
+    else:
+        print("ERROR")
 
-# if sc == 200:
-#   print(jsonPrint(response))
+for plyrid in ids:
+    response = requests.get(averagesUrl+str(plyrid))
+    sc = response.status_code
+    if sc == 200:
+        stats = json.loads(formatjson(response))['data'][0]
+        gmsc = stats["pts"] + (stats["fgm"]*0.4) + (stats["fga"]*( -0.7)) + ((stats["fta"]-stats["ftm"]) *(-0.4)) + (stats["oreb"]* 0.7) + (stats["dreb"] * 0.3) + stats["stl"] + (stats["ast"] * 0.7) + (stats["blk"] * 0.7) + (stats["pf"] *(-0.4)) - stats["turnover"]
+        seasonScores.append(gmsc)
+    else:
+        print("ERROR")
+
+sortedscores = sorted(seasonScores, reverse=True)
+
+for i in range(10):
+    print(str(i+1) + ". " + str(candidates[seasonScores.index(sortedscores[i])]) + " "+ str(sortedscores[i]))
+
+
